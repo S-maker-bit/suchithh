5080dc0e9413163d9d1b52963710f375f71c4d42
S.J.V.Suchith <24110313@iitgn.ac.in>
2024-11-15 21:59:03 +0530

    SC368

---
+++ hijava.js
@@ -0,0 +1,20 @@
+// const title= document.getElementById("13");
+// let age=parseInt(prompt("what is your age"));
+
+// if (age>18) {
+//     title.innerText="vote";
+//     title.style.color="green";
+// }
+// else{
+//     title.innerText="cannot vote";
+//     title.style.color="blue";
+// }
+// const inputText=document.getElementById("Todo title")
+// function onClickSubmitButton(event){
+//     event.preventDAefault();
+// }
+const inputText = document.getElementById("TodoTitle");
+const Todos = [];
+function onClickSubmitButton() {
+  console.log(inputText.value);
+}
